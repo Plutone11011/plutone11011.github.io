be0cd8709604684678ba1a0bb8dfe71b751812d3
Lorenzo Borelli <lorenzo.borelli27@gmail.com>
2025-02-21 13:20:27 +0100

    implemented backward function

--- ml-rust/backprop/src/main.rs
+++ ml-rust/backprop/src/main.rs
@@ -1,75 +1,99 @@
 
 use std::ops::{Add, Mul, Sub, Div};
 
+#[derive(Default)]
 enum Op {
     Add,
     Mult,
     Sub,
-    Div
+    Div,
+    // NoOp for leaf (input) nodes that are not composed from other functions
+    #[default]
+    NoOp
 }
 
+#[derive(Default)]
 struct Value {
     data: f64,
     children: Vec<Self>, // children of each value, e.g. a = b + c, b and c are children of a
-    op: Op
-}
+    op: Op,
+    grad: f64,
+    backward: Option<Box<dyn FnMut()>>
+} 
 
+
+impl Value {
+    // fn new(data: f64, children: Vec<Value>, op: Op) -> Self {
+
+    //     Value { data: data, children: children, op: op, grad: 0., backward: None }
+    
+    // }
+
+    fn set_children(&mut self, children: Vec<Value>) -> (){
+        self.children = children
+    }
+
+    fn set_op(&mut self, op: Op) -> (){
+        self.op = op
+    }
+    
+    fn set_gradient(&mut self, grad: f64) -> (){
+        self.grad = grad
+    }
+
+    fn set_data(&mut self, data: f64) -> (){
+        self.data = data
+    }
+
+    fn set_backward(&mut self, backward: Option<Box<dyn FnMut()>>) -> () {
+        self.backward = backward;
+    }
+}
 
 impl Add for Value {
     type Output = Value;
 
-    fn add(self, rhs: Self) -> Self::Output {
-        let out = Value {
-            data: self.data + rhs.data,
-            children: vec![self, rhs],
-            op: Op::Mult
+
+    fn add(mut self, mut rhs: Self) -> Self::Output {
+
+        let mut out = Value::default();
+
+
+        let backward = move || {
+            self.grad = 1. * out.grad;
+            rhs.grad = 1. * out.grad;
         };
+        out.set_backward(Some(Box::new(backward)));
+        out.set_data(self.data + rhs.data);
+        out.set_op(Op::Add);
+        out.set_children(vec![self, rhs]);
+        
         out
     }
 }
 
-impl Sub for Value {
-    type Output = Value;
-
-    fn sub(self, rhs: Self) -> Self::Output {
-        let out = Value {
-            data: self.data - rhs.data,
-            children: vec![self, rhs],
-            op: Op::Sub
-        };
-        out
-    }
-}
 
 impl Mul for Value {
     type Output = Value;
 
-    fn mul(self, rhs: Self) -> Self::Output {
-        let out = Value {
-            data: self.data * rhs.data,
-            children: vec![self, rhs],
-            op: Op::Add
+    fn mul(mut self, mut rhs: Self) -> Self::Output {
+        let mut out = Value::default();
+
+
+        let backward = move || {
+            self.grad = rhs.data * out.grad;
+            rhs.grad = self.data * out.grad;
         };
+        out.set_backward(Some(Box::new(backward)));
+        out.set_data(self.data * rhs.data);
+        out.set_op(Op::Mult);
+        out.set_children(vec![self, rhs]);
+        
         out
     }
 }
 
-impl Div for Value {
-    type Output = Value;
 
-    fn div(self, rhs: Self) -> Self::Output {
-        if rhs.data == 0. {
-            panic!("Cannot divide by 0");
-        }
-
-        let out = Value {
-            data: self.data / rhs.data,
-            children: vec![self, rhs],
-            op: Op::Add
-        };
-        out
-    }
-}
 
 fn main() {
     println!("Hello, world!");
